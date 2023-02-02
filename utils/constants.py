f662bbecf51ac5477bd83e4f914254014d58cf92
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2023-02-02 17:50:39 +0000

    chore: perspective api key warning

--- utils/constants.py
+++ utils/constants.py
@@ -1,29 +1,29 @@
 """Project's constant variables and API keys.
 
-TODO:
-- Check if these attributes are still the same.
-
 Heavily inspired by
 https://github.com/allenai/real-toxicity-prompts/blob/master/utils/constants.py
 """
 
 import os
+import warnings
 
 PERSPECTIVE_API_KEY = os.environ.get("PERSPECTIVE_API_KEY", None)
 if PERSPECTIVE_API_KEY is None:
-    raise AttributeError('Please run `export PERSPECTIVE_API_KEY=´key´ before running this file.`')
+    warnings.warn(
+        "Please run `export PERSPECTIVE_API_KEY=´key´ if you wish to use PerspectiveAPI."
+    )
 
 
 # All attributes can be found here:
 # https://github.com/conversationai/perspectiveapi/blob/master/2-api/models.md
 PERSPECTIVE_API_ATTRIBUTES = (
-    'TOXICITY',
-    'SEVERE_TOXICITY',
-    'IDENTITY_ATTACK',
-    'INSULT',
-    'THREAT',
-    'PROFANITY',
-    'SEXUALLY_EXPLICIT',
-    'FLIRTATION'
+    "TOXICITY",
+    "SEVERE_TOXICITY",
+    "IDENTITY_ATTACK",
+    "INSULT",
+    "THREAT",
+    "PROFANITY",
+    "SEXUALLY_EXPLICIT",
+    "FLIRTATION",
 )
 PERSPECTIVE_API_ATTRIBUTES_LOWER = tuple(a.lower() for a in PERSPECTIVE_API_ATTRIBUTES)
