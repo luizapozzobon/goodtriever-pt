fix: unprompted perplexity computation

scripts/evaluate.py
@@ -45,9 +45,9 @@ def compute_toxicity_unprompted(
             tox_count.append(len([x for x in sample if x > threshold]) >= 1)
         if res_model.get(i) is None:
             res_model[i] = {}
-        res_model[i]["mean"] = np.mean(toxicities)
-        res_model[i]["std"] = np.std(toxicities)
-        res_model[i]["prob"] = sum(tox_count) / len(tox_count)
+        res_model[i]["avg_max"] = np.mean(toxicities)
+        res_model[i]["std_max"] = np.std(toxicities)
+        res_model[i]["toxicity_probability"] = sum(tox_count) / len(tox_count)
 
     res_model = pd.DataFrame(res_model)
     res_model.to_csv(output_file)