d9cb47192d4c68dbf9b3b11a41c5b0c9804aed90
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2023-05-05 16:45:30 +0000

    chore: update prompted experiment params

--- experiments/prompted_experiment.py
+++ experiments/prompted_experiment.py
@@ -6,10 +6,10 @@ import numpy as np
 import torch
 
 
-def main(param="lambda", temperature=1, dstores="both", min_alpha=0.5, max_alpha=2.0, step=0.5):
+def main(param="lambda", temperature=1, dstores="both", min_alpha=0.5, max_alpha=2.0, step=0.5, model_name="gpt2-large", rate_limit=20):
 
-    toxic_dstore = "checkpoints/gpt2_jigsaw_toxic"
-    nontoxic_dstore = "checkpoints/gpt2_jigsaw_nontoxic"
+    toxic_dstore = "checkpoints/gpt2-large/gpt2_toxicity_gte0.5_clean"
+    nontoxic_dstore = "checkpoints/gpt2-large/gpt2_toxicity_eq0_half_clean"
 
     dstore_dirs = ""
     if dstores in ["toxic", "both"]:
@@ -19,6 +19,7 @@ def main(param="lambda", temperature=1, dstores="both", min_alpha=0.5, max_alpha
 
     cmd = """
         python -m scripts.run_all \
+            --model_name {model_name} \
             --prompts_path data/dexperts/prompts/nontoxic_prompts-10k.jsonl \
             --output_folder outputs/gpt2_jigsaw/ensemble/small_prompted_exp_patch_min/temperature_{temperature}/{param}/{dstores}/{param}_{value} \
             {dstore_dirs} \
@@ -27,17 +28,20 @@ def main(param="lambda", temperature=1, dstores="both", min_alpha=0.5, max_alpha
             --lmbda {value} \
             --knn_temp {temperature} \
             --num_prompts 100 \
+            --perspective_rate_limit {rate_limit} \
             --batch_size 4
     """
 
     for value in np.arange(min_alpha, max_alpha + step, step):
         print("\nRunning with {} = {}".format(param, value))
         filled_cmd = cmd.format(
+            model_name=model_name,
             param=param,
             value=value,
             temperature=temperature,
             dstore_dirs=dstore_dirs,
             dstores=dstores,
+            rate_limit=rate_limit,
         )
         os.system(filled_cmd)
         torch.cuda.empty_cache()
