043dcc2f7c5c31eb224ec03720ffe85b03d1f0c0
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2023-07-06 19:20:26 +0000

    docs: arguments description

--- generation/args.py
+++ generation/args.py
@@ -24,81 +24,155 @@ class GenerationParser:
 @dataclass
 class GenerationArguments:
     """
-    Args:
-        output_folder (str, optional): Folder to save results.
-        prompts_path (str, optional): Prompts filename.
-            Defaults to "gs://cohere-dev/data/realtoxicityprompts/prompts.jsonl".
-        model_name (str, optional): Model to use from HuggingFace Hub.
-            Defaults to "gpt2".
-        num_return_sequences (int, optional): Number of sequences to return for each prompt.
-            Defaults to 25. If `use_eos`, hard-coded to 1.
-        max_new_tokens (int, optional): Number of tokens to generate. Defaults to 20.
-        top_p (float, optional): Top p for nucleus sampling. Defaults to 0.90.
-        batch_size (int, optional): Tokenization and generation batch size. Defaults to 16.
-        use_eos (bool, optional): Whether to do an unprompted generation of not.
-            If True, ignores `filename` prompts and generates `eos_samples` sequences from
-            an end of sequence token. Defaults to False.
-        eos_samples (int, optional): Number of eos generations. Defaults to 10k
-        output_filename (str, optional): Output filename. If None, will be built
-            automatically from user parameters. Defaults to None.
-        num_prompts (int, optional): Number of prompts to use. If None, will use all.
+    Arguments pertaining general generation arguments.
     """
 
     output_folder: str
-    prompts_path: str = field(default="gs://cohere-dev/data/realtoxicityprompts/prompts.jsonl")
-    model_name: str = field(default="gpt2")
-    num_return_sequences: int = field(default=25)
-    max_new_tokens: int = field(default=20)
-    top_p: float = field(default=0.90)
-    batch_size: int = field(default=16)
-    use_eos: bool = field(default=False)
-    eos_samples: int = field(default=10_000)
-    output_filename: str = field(default=None)
-    num_prompts: int = field(default=None)
+    prompts_path: str = field(
+        default="gs://cohere-dev/data/realtoxicityprompts/prompts.jsonl",
+        metadata={"help": "Prompts filename."},
+    )
+    model_name: str = field(
+        default="gpt2-large",
+        metadata={"help": "Model to use from HuggingFace Hub. " "Defaults to 'gpt2-large'. "},
+    )
+    num_return_sequences: int = field(
+        default=25,
+        metadata={
+            "help": "Number of sequences to return for each prompt. "
+            "Defaults to 25. If `use_eos`, hard-coded to 1."
+        },
+    )
+    max_new_tokens: int = field(
+        default=20, metadata={"help": "Number of tokens to generate. Defaults to 20."}
+    )
+    top_p: float = field(
+        default=0.90,
+        metadata={"help": "Top-p for nucleus sampling (after ensemble). Defaults to 0.90."},
+    )
+    batch_size: int = field(
+        default=16, metadata={"help": "Tokenization and generation batch size. Defaults to 16."}
+    )
+    use_eos: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to do an unprompted generation of not. "
+            "If True, ignores `filename` prompts and generates `eos_samples` sequences from "
+            "an end of sequence token. Defaults to False."
+        },
+    )
+    eos_samples: int = field(
+        default=10_000, metadata={"help": "Number of eos generations. Defaults to 10k"}
+    )
+    output_filename: str = field(
+        default=None,
+        metadata={
+            "help": "Output filename. If None, will be built "
+            "automatically from user parameters. Defaults to None."
+        },
+    )
+    num_prompts: int = field(
+        default=None, metadata={"help": "Number of prompts to use. If None, will use all."}
+    )
 
 
 @dataclass
 class KNNArguments:
     """
-    Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
+    Arguments pertaining retrieval-augmentation with kNN. Also supports DExperts experiments.
     """
 
-    knn: bool = field(default=False)
-    dexperts: bool = field(default=False)
-    knn_gpu: bool = field(default=True)
-    dstore_size: int = field(default=None, metadata={"help": "The size of the dstore."})
-    knn_keytype: KEY_TYPE.from_string = field(default=KEY_TYPE.last_ffn_input)
-    save_knnlm_dstore: bool = field(default=False)
-    dstore_dir: str = field(default="knn_transformers/checkpoints")
-    other_dstore_dir: str = field(default=None)
-    knn_sim_func: DIST.from_string = field(default=DIST.l2)
-    lmbda: float = field(default=0.5)
-    k: int = field(default=1024)
-    other_k: int = field(default=None)
-    knn_temp: float = field(default=100)
-    # Args for building the faiss index:
-    build_index: bool = field(default=False)
-    # To use flat index instead of quantized index in Faiss, set to True
-    # Flat index is slower but returns 100% accurate nearest neighbor results
-    flat_index: bool = field(default=False)
-    # faiss_index: str = field(default="checkpoints/index")
-    ncentroids: int = field(default=4096)
-    code_size: int = field(default=64)
-    probe: int = field(default=32)
-    num_keys_to_add_at_a_time: int = field(default=1000000)
-    move_dstore_to_mem: bool = field(default=True)
-    no_load_keys: bool = field(default=True)
-    recompute_dists: bool = field(default=False)
-    method: str = field(default="interpolation")
-    filter_p: float = field(default=0.9)
-    ensemble_order: tuple = field(default=("subtract", "add"))
-
-    ## RetoMaton args:
-    retomaton: bool = field(default=False)
-    cluster_dstore: bool = field(default=False)
-    no_pointer: bool = field(default=False)
-    min_knns: int = field(default=1)
-    max_knns: int = field(default=1024)
-    num_clusters: int = field(default=500000)
-    sample_size: int = field(default=20000000)
-    members: str = field(default=None)
+    knn: bool = field(default=False, metadata={"help": "To retrieve or not to retrieve."})
+    dexperts: bool = field(
+        default=False,
+        metadata={
+            "help": "To use DExperts or not. If `knn` is True, that supersedes this argument."
+        },
+    )
+    dstore_size: int = field(
+        default=None,
+        metadata={
+            "help": "The size of the dstore. If None (recommended), it will be detected automatically."
+        },
+    )
+    knn_gpu: bool = field(default=True, metadata={"help": "To run kNN search on GPU or not."})
+    knn_keytype: KEY_TYPE.from_string = field(
+        default=KEY_TYPE.last_ffn_input,
+        metadata={"help": "The Key Type points to the layer to extract the datastore key from."},
+    )
+    dstore_dir: str = field(
+        default="checkpoints", metadata={"help": "The directory of the first dstore (toxic)."}
+    )
+    other_dstore_dir: str = field(
+        default=None, metadata={"help": "The directory of the second dstore (non-toxic)."}
+    )
+    k: int = field(
+        default=1024, metadata={"help": "The number of retrieved neighbors from the first dstore."}
+    )
+    other_k: int = field(
+        default=None,
+        metadata={"help": "The number of retrieved neighbors from the second dstore."},
+    )
+    knn_sim_func: DIST.from_string = field(
+        default=DIST.l2, metadata={"help": "kNN search similarity function."}
+    )
+    lmbda: float = field(
+        default=2.0, metadata={"help": "Ensemble coefficient (alpha in the paper)."}
+    )
+    knn_temp: float = field(
+        default=100,
+        metadata={"help": "kNN temperature. The larger, the flatter the distribution."},
+    )
+    build_index: bool = field(
+        default=False,
+        metadata={"help": "If True, we'll train/save the FAISS index to disk."},
+    )
+    save_knnlm_dstore: bool = field(
+        default=False, metadata={"help": "If True, saves datastores to disk."}
+    )
+    flat_index: bool = field(
+        default=False,
+        metadata={
+            "help": "To use a flat index instead of the quantized (FAISS). "
+            "Flat index is slower, but is totally accurate."
+        },
+    )
+    ncentroids: int = field(
+        default=4096, metadata={"help": "Number of centroids for quantized FAISS index."}
+    )
+    code_size: int = field(
+        default=64,
+        metadata={"help": "The code_size is typically a power of two between 4 and 64."},
+    )
+    probe: int = field(default=32, metadata={"help": "Number of probes at query time."})
+    num_keys_to_add_at_a_time: int = field(
+        default=1000000, metadata={"help": "Number of keys to add to index at a time."}
+    )
+    move_dstore_to_mem: bool = field(
+        default=True, metadata={"help": "If True, loads keys and values to memory."}
+    )
+    no_load_keys: bool = field(
+        default=True,
+        metadata={
+            "help": "If True, keys are not loaded to memory. Supersedes `move_dstore_to_mem`."
+        },
+    )
+    recompute_dists: bool = field(
+        default=False,
+        metadata={
+            "help": "Recompute the distances of retrieved neighbors for accurate probability computation."
+        },
+    )
+    method: str = field(
+        default="ensemble",
+        metadata={
+            "help": "Ensemble method. Choices: `ensemble`, `interpolate`, `interpolate_discourage`"
+        },
+    )
+    filter_p: float = field(default=0.9, metadata={"help": "Top-p filtering (before ensemble)."})
+    ensemble_order: tuple = field(
+        default=("subtract", "add"),
+        metadata={
+            "help": "Order of datastores on ensemble equation. By default, the first is subtracted."
+        },
+    )
